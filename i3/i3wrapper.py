6215f87e69e423c9d24de968617dcf77c327fe64
Alessandro Piras <laynor@gmail.com>
2017-05-10 12:36:31 +0200

    use template for i3 config

---
+++ i3/i3wrapper.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import os
+import pystache
+import sys
+import configparser
+
+HOME = os.getenv("HOME")
+
+def updateConfig():
+    conf = configparser.ConfigParser()
+    conf.read(os.path.join(HOME, '.i3/config.ini'))
+    font = conf.get('appearance', 'font')
+    templateName = os.path.join(HOME,".i3/config.mustache" )
+    outputName = os.path.join(HOME, ".i3/config")
+
+    renderer = pystache.Renderer()
+
+    with open(templateName, 'r') as tf:
+        with open(outputName, 'w+') as of:
+            template = tf.read()
+            parsed = pystache.parse(template)
+            rendered = renderer.render(template, {'font': font})
+            print(rendered, file=of)
+
+
+if __name__ == '__main__':
+    updateConfig()
+    if len(sys.argv) == 1:
+        os.system('i3')
+    if sys.argv[1] == 'reload':
+        os.system('i3-msg reload')

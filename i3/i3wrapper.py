d008eeb63b27da3b322487ded144be89e0f12e65
Alessandro Piras <laynor@gmail.com>
2017-05-11 00:38:19 +0200

    cleanup i3 configuration

--- i3/i3wrapper.py
+++ i3/i3wrapper.py
@@ -4,29 +4,39 @@ import pystache
 import sys
 import configparser
 
-HOME         = os.getenv("HOME")
-I3_DIR       = os.path.join(HOME, '.i3')
-TEMPLATE_DIR = os.path.join(I3_DIR, 'config.d')
-INI_FILE     = os.path.join(I3_DIR, 'config.ini')
-CONFIG_FILE  = os.path.join(I3_DIR, 'config')
+HOME          = os.getenv("HOME")
+I3_DIR        = os.path.join(HOME, '.i3')
+TEMPLATE_DIR  = os.path.join(I3_DIR, 'config.d')
+CONF_INI      = os.path.join(I3_DIR, 'config.ini')
+CONFIG        = os.path.join(I3_DIR, 'config')
+I3STATUS_CONF = os.path.join(HOME, '.i3status.conf')
 
 def getIniVariables():
     conf = configparser.ConfigParser()
-    conf.read(INI_FILE)
+    conf.read(CONF_INI)
     font = conf.get('appearance', 'font')
     return { 'font': font }
 
-def writeOutputFile(rendered):
-    with open(CONFIG_FILE, 'w+') as of:
-        print(rendered, file=of)
+def writeConfiguration(i3Conf, i3StatusConf):
+    with open(CONFIG, 'w+') as of:
+        print(i3Conf, file=of)
+    with open(I3STATUS_CONF, 'w+') as of:
+        print(i3StatusConf, file=of)
 
-
-def updateConfig():
+def renderI3Conf():
     renderer = pystache.Renderer()
     renderer.search_dirs = [TEMPLATE_DIR]
-    rendered = renderer.render('{{> config}}', getIniVariables())
+    return renderer.render('{{> config}}', getIniVariables())
 
-    writeOutputFile(rendered)
+def renderI3StatusConf():
+    renderer = pystache.Renderer()
+    renderer.search_dirs = [TEMPLATE_DIR]
+    return renderer.render('{{> i3status.conf}}', getIniVariables())
+
+def updateConfig():
+    i3Conf = renderI3Conf()
+    i3StatusConf = renderI3StatusConf()
+    writeConfiguration(i3Conf, i3StatusConf)
 
 
 if __name__ == '__main__':

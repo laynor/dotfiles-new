93621a875bd32c2dad93f3c8785a2b5601c636dc
Alessandro Piras <laynor@gmail.com>
2026-06-24 17:05:33 +0900

    Add tmux-powerline config

---
+++ config/tmux-powerline/config.sh
@@ -0,0 +1,4 @@
+# tmux-powerline user config. Sourced before defaults are applied.
+# Refresh interval (seconds). Default of 1 spawns a bash+nu segment tree
+# per window every second -> heavy process churn. 15 is tmux's own default.
+export TMUX_POWERLINE_STATUS_INTERVAL=15
